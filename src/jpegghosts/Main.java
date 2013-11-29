211b737cc87d5d274e2c9c2a46df17bebc4f87b9
rodrigoegoncalves <rodrigoegoncalves@96b08fd9-a135-b6ef-b51a-81c68b2004cc>
2013-11-29 19:34:09 +0000

    code

--- src/jpegghosts/Main.java
+++ src/jpegghosts/Main.java
@@ -1,8 +1,10 @@
 package jpegghosts;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -11,14 +13,15 @@ public class Main {
     // http://docs.opencv.org/java/
 	static { System.loadLibrary("opencv_java246"); }
 	
-    public static void main(String[] args) {
+	private static double TRESHOlD = 0.76;
+	
+    public static void main(String[] args) throws FileNotFoundException {
     	System.out.println("Starting");
     	//generateDataSet();
     	
-    	runMethod();
-		
-		
+    	//runMethod();
     	
+    	identifyTamper();
     }
 
 	private static void runMethod() {
@@ -51,6 +54,53 @@ public class Main {
 	private static void generateDataSet() {
 		DataSetGenerate dataSet = new DataSetGenerate("generate_dataset_original_images", "source_images");
     	dataSet.generate();
+	}
+	
+	private static void identifyTamper() throws FileNotFoundException {
+		File folder = new File("result_images");
+		File[] imagesNames = folder.listFiles();
+    	
+		JpegGhosts jpegGhosts = new JpegGhosts();
+		
+		Arrays.sort(imagesNames);
+		for (final File imageFolder : imagesNames) {
+			if (imageFolder.isDirectory()) {
+				
+				File[] files = imageFolder.listFiles(new FilenameFilter() {
+		    	    public boolean accept(File dir, String name) {
+		    	    	return name.toLowerCase().endsWith(".png");
+		    	    }
+		    	});
+				
+				boolean tampered = false;
+				Scanner scanner = new Scanner(new File("source_images/" + imageFolder.getName() + ".txt"));
+				int width = scanner.nextInt();
+		    	int height = scanner.nextInt();
+		    	int x = scanner.nextInt();
+		    	int y = scanner.nextInt();
+		    	scanner.close();
+
+		    	Arrays.sort(files);
+				for (final File fileEntry : files) {
+					if (!fileEntry.isDirectory()) {
+						double difference = jpegGhosts.detectTamper(fileEntry, width, height, x, y);
+						System.out.println(fileEntry.getName());
+						System.out.println(difference);
+						if (difference > TRESHOlD) {
+							tampered = true;
+						}
+					}
+				}
+				
+				if (tampered) {
+					System.out.println(imageFolder.getName() + " is modified");
+				}
+				else {
+					System.out.println(imageFolder.getName() + " is not modified");
+				}
+				
+			}
+		}
 	}
     
 }
